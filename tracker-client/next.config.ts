b3b04c998e1ea830137dd24ea0dd1cedddf26ffd
JayanithyaMadhushani <95066839+DULAJBHAGYA@users.noreply.github.com>
2025-06-18 15:08:09 +0530

    change next configurations

--- tracker-client/next.config.ts
+++ tracker-client/next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        // port: '', // Only include if you need to specify a port
+        pathname: '/**', // This allows all paths under the domain
+      },
+      // Add more patterns as needed for other image sources
+    ],
+  },
+  // ... other configurations
+}
 
-export default nextConfig;
+export default nextConfig
